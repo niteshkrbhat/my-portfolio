a501dc2ce4dca5523664afa334cd752f6cdce6db
MacTavish007 <nitesh@solultima.com>
2023-02-16 19:10:58 +0530

    minor js removed

--- assets/js/common.js
+++ assets/js/common.js
@@ -470,45 +470,6 @@ function initCursor() {
 	function lerp (start, end, amt) {
 		return (1-amt)*start+amt*end
 	}
-
-	/*
-		Validate Contact Form
-	*/
-	if($('.contacts-form').length) {
-	$('#cform').validate({
-		rules: {
-			name: {
-				required: true
-			},
-			message: {
-				required: true
-			},
-			email: {
-				required: true,
-				email: true
-			}
-		},
-		success: 'valid',
-		submitHandler: function() {
-			$.ajax({
-				url: 'mailer/feedback.php',
-				type: 'post',
-				dataType: 'json',
-				data: 'name='+ $("#cform").find('input[name="name"]').val() + '&email='+ $("#cform").find('input[name="email"]').val() + '&subject='+ $("#cform").find('input[name="subject"]').val() + '&message=' + $("#cform").find('textarea[name="message"]').val(),
-				beforeSend: function() {
-
-				},
-				complete: function() {
-
-				},
-				success: function(data) {
-					$('#cform').fadeOut();
-					$('.alert-success').delay(1000).fadeIn();
-				}
-			});
-		}
-	});
-	}
 }
 
 function setHeightFullSection() {
